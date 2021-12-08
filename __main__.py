b34c28cb302ad557e0a721a9e9fa73f419df4f09
Tony Wecker <26899356+twecker137@users.noreply.github.com>
2021-12-08 13:33:54 +0100

    add initial files

---
+++ __main__.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import json
+import pulumi
+from pulumi import automation as auto, ResourceOptions
+import pulumi_azure_native as azure_native
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+import LandingZone
+
+dotenv_path = Path('dev.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+admin_user = os.getenv('ADMIN_USER')
+admin_ssh_pubkey = os.getenv('ADMIN_SSH_PUBKEY')
+access_source_address_prefix = os.getenv('ACCESS_SOURCE_ADDRESS_PREFIX')
+
+
+# To destroy our program, we can run python main.py destroy
+destroy = False
+# To preview our program, we can run python main.py preview
+preview = False
+args = sys.argv[1:]
+if len(args) > 0:
+    if args[0] == "destroy":
+        destroy = True
+    elif args[0] == "preview":
+        preview = True
+
+project_name = "azure-kubernetes"
+# We use a simple stack name here, but recommend using auto.fully_qualified_stack_name for maximum specificity.
+stack_name = "dev"
+# stack_name = auto.fully_qualified_stack_name("myOrgOrUser", project_name, stack_name)
+
+# create or select a stack matching the specified name and project.
+# this will set up a workspace with everything necessary to run our inline program (pulumi_program)
+stack = auto.create_or_select_stack(stack_name=stack_name,
+                                    project_name=project_name,
+                                    program=pulumi_program)
+
+print("successfully initialized stack")
+
+# for inline programs, we must manage plugins ourselves
+print("installing plugins...")
+stack.workspace.install_plugin("azure-native", "v1.23.0")
+print("plugins installed")
+
+# set stack configuration specifying the Azure region to deploy
+print("setting up config")
+stack.set_config("azure-native:location", auto.ConfigValue(value=os.getenv('AZURE_LOCATION')))
+
+print("config set")
+
+print("refreshing stack...")
+stack.refresh(on_output=print)
+print("refresh complete")
+
+if destroy:
+    print("destroying stack...")
+    stack.destroy(on_output=print)
+    print("stack destroy complete")
+    sys.exit()
+
+if preview:
+    print("stack preview")
+    up_res = stack.preview(on_output=print)
+    print(f"preview summary: \n{json.dumps(up_res.change_summary, indent=4)}")
+    sys.exit()
+
+print("updating stack...")
+up_res = stack.up(on_output=print)
+print(f"update summary: \n{json.dumps(up_res.summary.resource_changes, indent=4)}")
